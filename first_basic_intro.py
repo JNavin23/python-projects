0f007c78ccf260b1346d61d848f4ba81453ce6d2
Navin <naveenjoshi272@gmail.com>
2025-05-12 11:29:04 +0530

    Added first basic intro script.

---
+++ first_basic_intro.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ").title()
+age = int(input("Enter your age: "))
+place = input("Where do you live: ").title()
+print(f"Hi {name}! You turned {age} years old.\nAnd you live in {place}.")
